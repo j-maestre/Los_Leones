b655958d590f9338636429a8581168eaea43bff4
j-maestre <xemaiestacio@gmail.com>
2021-10-22 14:10:12 +0200

    add products menu

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -2,14 +2,13 @@ import React from "react";
 // import { Geolocation } from '@ionic-native/geolocation';
 
 import "./Products.css";
+import "../css/bootstrap.min.css"
 // import EventList from "../components/Event/Event_List";
 
 import Header from "../components/header/HeaderComponent";
 import Footer from "../components/footer/Footer";
 
 import { useTranslation } from "react-i18next";
-
-console.log("HOMMMMMMMMMMMME")
 
 const Products = () => {
   // const { dispatch } = useContext(AppContext);
@@ -18,12 +17,20 @@ const Products = () => {
   return (
     <section className="products">
       <Header page={t("pages.products")} />
-      <p>Esto es el products</p>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <p>ole los products</p>
+      <div className="section-title text-center">
+        <h1>Productos</h1>
+        <p>Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam risus.</p>
+      </div>
+
+      <div className="products__menu">
+					<button className="products__menu-item" id="all">Todo</button>
+					<button className="products__menu-item" id="sillas">Sillas</button>
+					<button className="products__menu-item" id="armarios">Armarios</button>
+					<button className="products__menu-item" id="mesas">Mesas</button>
+					<button className="products__menu-item" id="estanterias">Estanterias</button>			
+			</div>
+      
+      
       <Footer/>
     </section>
   );

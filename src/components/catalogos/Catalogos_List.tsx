1fcb628db80ed7ba89b9ea3ebc665297d69739db
j-maestre <xemaiestacio@gmail.com>
2022-01-24 15:22:32 +0100

    add download url

--- src/components/catalogos/Catalogos_List.tsx
+++ src/components/catalogos/Catalogos_List.tsx
@@ -28,25 +28,25 @@ const Catalogos_List: React.FC<CatalogosProps> = (props) => {
       <hr></hr>
       <section className="catalogs_links">
         <h3>Belfast</h3>
-        <a href="../../data/catalogs/BELFAST2021.pdf" download="BELFAST2021.pdf">Download</a>
+        <a href="https://github.com/j-maestre/Los_Leones/raw/master/src/data/catalogs/BELFAST2021.pdf" download="BELFAST2021.pdf">Download</a>
         <hr></hr>
         <h3>Cosmo</h3>
-        <a href="path_to_file" download="proposed_file_name">Download</a>
+        <a href="https://github.com/j-maestre/Los_Leones/raw/master/src/data/catalogs/Catalogo_Eos_Cosmo_2020.pdf" download="Catalogo_Eos_Cosmo_2020">Download</a>
         <hr></hr>
         <h3>Formas</h3>
-        <a href="path_to_file" download="proposed_file_name">Download</a>
+        <a href="https://github.com/j-maestre/Los_Leones/raw/master/src/data/catalogs/Catalogo_Formas_19.pdf" download="Catalogo_Formas_19.pdf">Download</a>
         <hr></hr>
         <h3>Basic</h3>
-        <a href="path_to_file" download="proposed_file_name">Download</a>
+        <a href="https://github.com/j-maestre/Los_Leones/raw/master/src/data/catalogs/DORMITORIOS_BASIC.pdf" download="DORMITORIOS_BASIC">Download</a>
         <hr></hr>
         <h3>Forest</h3>
-        <a href="path_to_file" download="proposed_file_name">Download</a>
+        <a href="https://github.com/j-maestre/Los_Leones/raw/master/src/data/catalogs/FOREST_2021.pdf" download="FOREST_2021.pdf">Download</a>
         <hr></hr>
         <h3>Mood Ros</h3>
-        <a href="path_to_file" download="proposed_file_name">Download</a>
+        <a href="https://github.com/j-maestre/Los_Leones/raw/master/src/data/catalogs/MOOD_ROS_MINI_2020.pdf" download="MOOD_ROS_MINI_2020.pdf">Download</a>
         <hr></hr>
         <h3>Seres</h3>
-        <a href="path_to_file" download="proposed_file_name">Download</a>
+        <a href="https://github.com/j-maestre/Los_Leones/raw/master/src/data/catalogs/SERES_Cat%C3%A1logo%202020.pdf" download="SERES_Catálogo 2020.pdf">Download</a>
         <hr></hr>
 
       </section>
